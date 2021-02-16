8275e595000dc3b298b9baea8a3baf11a9cea176
Brian Murphy <blpmurphy@gmail.com>
2021-02-16 12:43:08 +0000

    Customise admin fields

--- products/admin.py
+++ products/admin.py
@@ -3,5 +3,26 @@ from .models import Category, Product
 
 
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'category',
+        'sku',
+        'name',
+        'price',
+        'image',
+        'rating',
+    )
+
+    ordering = ('category',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
