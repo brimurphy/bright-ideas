39537ce3f1234844049488b9c9a77b1bb5c36b61
Brian Murphy <blpmurphy@gmail.com>
2021-02-16 12:15:33 +0000

    Add products app and product images to media

---
+++ products/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    ip_rating = models.CharField(max_length=4, null=True, blank=True)
+    bulb_type = models.CharField(max_length=20, null=True, blank=True)
+    dimmable = models.BooleanField(default=False)
+    clearance = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
