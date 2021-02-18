20035dc3e8c2aa33a6fbae4f3b8da3e993acc415
Brian Murphy <blpmurphy@gmail.com>
2021-02-18 15:49:58 +0000

    Update context processor for adding items to bag

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,5 +1,8 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+
+from products.models import Product
 
 
 def cart_contents(request):
@@ -9,6 +12,16 @@ def cart_contents(request):
     product_count = 0
     free_delivery = settings.FREE_DELIVERY_THRESHOLD
     delivery_cost = settings.STANDARD_DELIVERY_PERCENTAGE
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        cart_items.append({
+           'item_id': item_id,
+           'quantity': quantity,
+           'product': product,
+        })
 
     if total < free_delivery:
         delivery = total * Decimal(delivery_cost / 100)
