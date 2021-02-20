e827455bbed8a409a75e81cb0ac6d2922475edd8
Brian Murphy <blpmurphy@gmail.com>
2021-02-20 16:06:20 +0000

    Add Admin models for checkout app

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = (
+        'order_number', 'date', 'delivery_cost', 'order_total', 'grand_total')
+
+    fields = (
+        'order_number', 'full_name', 'email', 'phone_number',
+        'street_address1', 'street_address2', 'town_or_city',
+        'county', 'postcode', 'country', 'date', 'delivery_cost',
+        'order_total', 'grand_total',)
+
+    list_display = (
+        'order_number', 'date', 'full_name', 'delivery_cost',
+        'order_total', 'grand_total')
+
+    ordering = ('-date',)
+
+    admin.site.empty_value_display = '(None)'
+
+
+admin.site.register(Order, OrderAdmin)
