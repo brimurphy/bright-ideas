117e24752bde6b67f5611a318dda14b08fdb3cab
Brian Murphy <blpmurphy@gmail.com>
2021-02-26 00:43:10 +0000

    Remove trailing slash from allowed hosts

--- bright_ideas/settings.py
+++ bright_ideas/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['bright-ideas-bm.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['bright-ideas-bm.herokuapp.com', 'localhost']
 
 
 # Application definition
