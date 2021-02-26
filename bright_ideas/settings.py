7fe76680aa4717ad8a565713b0000567cc0651f7
Brian Murphy <blpmurphy@gmail.com>
2021-02-26 13:33:33 +0000

    Add real email responses

--- bright_ideas/settings.py
+++ bright_ideas/settings.py
@@ -110,9 +110,6 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-# Temp log emails to console
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 WSGI_APPLICATION = 'bright_ideas.wsgi.application'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
@@ -218,4 +215,14 @@ STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 DEFAULT_FROM_EMAIL = 'brightideas@example.com'
