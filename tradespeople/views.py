2efcc2c00907a9b6fa11dbe0828ebfed64eb82d4
Brian Murphy <blpmurphy@gmail.com>
2021-02-26 22:36:06 +0000

    Update body and subject in booking email confirmation

--- tradespeople/views.py
+++ tradespeople/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.core.mail import send_mail
 from django.conf import settings
+from django.template.loader import render_to_string
+
 
 from .models import TradesPeople
 from .forms import BookingForm
@@ -22,12 +24,22 @@ def trades_people(request):
     return render(request, template, context)
 
 
-def booking_email(userInfo):
-    cust_email = userInfo.default_email
+def booking_email(user_info, trades_person_name, date):
+    cust_email = user_info.default_email
+
+    subject = render_to_string(
+        'confirmation_emails/confirmation_booking_email_subject.txt',
+        {'trades_person': trades_person_name})
+    body = render_to_string(
+            'confirmation_emails/confirmation_booking_email_body.txt',
+            {'user_name': user_info.default_full_name,
+             'contact_email': settings.DEFAULT_FROM_EMAIL,
+             'date': date,
+             'trades_person': trades_person_name})
 
     send_mail(
-        'Test Email',
-        'This is a test for booking',
+        subject,
+        body,
         settings.DEFAULT_FROM_EMAIL,
         [cust_email]
     )
@@ -36,26 +48,25 @@ def booking_email(userInfo):
 @login_required
 def booking_form(request, person_id):
     # A view to book a tradesperson
-    user_Info = get_object_or_404(UserProfile, user=request.user)
-    trades_booking = get_object_or_404(TradesPeople, pk=person_id)
+    user_info = get_object_or_404(UserProfile, user=request.user)
+    trades_person = get_object_or_404(TradesPeople, pk=person_id)
 
     if request.method == 'POST':
-        form = BookingForm(request.POST, instance=user_Info)
+        date = request.POST.get('date')
+        form = BookingForm(request.POST, instance=user_info)
         if form.is_valid():
-            form.save()
             messages.success(request, 'Booking Complete')
-            booking_email(user_Info)
+            booking_email(user_info, trades_person.name, date)
             return redirect(reverse('products'))
         else:
             messages.error(request, 'Booking Failed!!')
     else:
-        form = BookingForm(instance=user_Info)
+        form = BookingForm(instance=user_info)
 
     template = 'tradespeople/booking_form.html'
     context = {
-        'trades_booking': trades_booking,
+        'trades_person': trades_person,
         'form': form,
-        'user_Info': user_Info,
     }
 
     return render(request, template, context)
