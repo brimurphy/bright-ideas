0557b44980486a0e12f78cbad723bb60a73902b7
Brian Murphy <blpmurphy@gmail.com>
2021-02-28 01:28:18 +0000

    Add add trade reviews view

--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.http import JsonResponse
 from django.contrib import messages
-
+from django.contrib.auth.decorators import login_required
 
 from .models import Reviews
 from .forms import ReviewForm
@@ -14,3 +14,50 @@ def reviews(request, trades_person_id):
     total = sum(review.rating for review in reviews)
     avg = total // reviews.count()
     return JsonResponse({'avg': avg, 'reviews': list(reviews.values())},)
+
+
+@login_required
+def add_reviews(request):
+    # Allow users to post reviews on tradespeople
+    reviews = Reviews.objects.all().values()
+
+    if request.method == 'POST':
+        form = ReviewForm(request.POST, request.FILES)
+        if form.is_valid():
+            review = form.save()
+            messages.success(request, 'New review added!')
+            return redirect(reverse('tradespeople',))
+        else:
+            messages.error(request, 'Failed to add review!')
+    else:
+
+        template = 'reviews/add_reviews.html'
+        context = {
+            'reviews': reviews,
+        }
+
+        return render(request, template, context)
+
+@login_required
+def add_trades_person_review(request, trades_person_id):
+    # Allow users to post reviews on tradespeople
+    reviews = Reviews.objects.select_related('user').select_related('trades_person').all()
+    print(Reviews.objects.select_related('user').select_related('trades_person').all().query)
+    form = ReviewForm(request.POST)
+    if request.method == 'POST':
+        if form.is_valid():
+            review = form.save()
+            messages.success(request, 'New review added!')
+            return redirect(reverse('tradespeople',))
+        else:
+            messages.error(request, 'Failed to add review!')
+    else:
+
+        template = 'reviews/add_reviews.html'
+        context = {
+            'reviews': reviews,
+            'trades_person_id': trades_person_id,
+            'form': form,
+        }
+
+        return render(request, template, context)
